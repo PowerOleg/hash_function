0404963c9eb89ce1f9b4598b256b8ddca121afa3
PowerOleg <geraskin.oleg@mail.ru>
2024-05-04 20:20:20 +0300

    task2 is done

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -12,9 +12,32 @@ int simple_string_hash(std::string text)
 	return sum;
 }
 
+int real_string_hash(std::string text, int p, int modul)
+{
+	int hash = 0;
+	int degree = 1;
+	for (size_t i = 0; i < text.length(); i++)
+	{
+		char ch = text[i];
+		int ch_int = static_cast<int>(ch);
+		int po = pow(p, i);
+		hash += ch_int * (po);
+
+	}
+	hash = hash % modul;
+	return hash;
+}
+
 int main(int argc, char** argv)
 {
 	setlocale(LC_ALL, "ru");
+	int p = 0;
+	printf("Введите p: ");
+	std::cin >> p;
+	int modul = 0;
+	printf("Введите n: ");
+	std::cin >> modul;
+
 	std::string text = "";
 	while (true)
 	{
@@ -24,9 +47,8 @@ int main(int argc, char** argv)
 		{
 			break;
 		}
-		int hashcode = simple_string_hash(text);
-
-		printf("Наивный хэш строки %s = %d\n", text.c_str(), /*std::to_string(*/ hashcode/*).c_str()*/);
+		int hashcode = real_string_hash(text, p, modul);
+		printf("Хэш строки %s = %d\n", text.c_str(), /*std::to_string(*/ hashcode/*).c_str()*/);
 	}
 	return 0;
 }
